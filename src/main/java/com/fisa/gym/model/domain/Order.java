78639fa1e561b298d979e3fab3687437b86959aa
popododo0720 <popododo0720@naver.com>
2024-01-02 12:14:43 +0900

    [Add] dto list로 변경

--- src/main/java/com/fisa/gym/model/domain/Order.java
+++ src/main/java/com/fisa/gym/model/domain/Order.java
@@ -1,10 +1,11 @@
 package com.fisa.gym.model.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -25,11 +26,9 @@ public class Order {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long orderId;
 	
-	@ManyToOne
-	@JoinColumn(name="memberId", referencedColumnName="memberId")
-	private Member member;
+	@ManyToOne(mappedBy = "memberId")
+	private List<Member> = members;
 	
-	@OneToMany
-	@JoinColumn(name="drinkId", referencedColumnName="drinkId")
-	private Drink drink;
+	@OneToMany(mappedBy = "drinkId")
+	private List<Drink> drinks;
 }
