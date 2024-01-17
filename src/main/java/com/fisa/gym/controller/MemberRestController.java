f4da02aa7e657b8e88c448562f074a73a46edbb0
popododo0720 <popododo0720@naver.com>
2024-01-17 17:35:10 +0900

    푸쉬

--- src/main/java/com/fisa/gym/controller/MemberRestController.java
+++ src/main/java/com/fisa/gym/controller/MemberRestController.java
@@ -2,11 +2,14 @@ package com.fisa.gym.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fisa.gym.model.domain.LoginRequest;
 import com.fisa.gym.service.MemberService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MemberRestController {
@@ -14,13 +17,18 @@ public class MemberRestController {
 	@Autowired
 	MemberService service;
 	
-	@PostMapping("/memberlogin")
-	public String member(LoginRequest req) throws Exception {
-		System.out.println("restcontroller======================" + req);
-//		return service.login(req);
-		if(service.login(req))
-			return "redirect:/afterlogin.jsp";
-		return "redirect:/nologin.jsp";
+	@RequestMapping(path = "/memberlogin", method = RequestMethod.POST)
+	public String member(String loginId, String password ,HttpSession session, Model model) throws Exception {
+		if(service.login(loginId,password)) {
+			
+			session.setAttribute("memberLoginId",loginId);
+			session.setAttribute("memberLoginPassword",password);
+			String memberLoginId = (String) session.getAttribute("memberLoginId");
+//			model.addAttribute("memberLoginId",memberLoginId);
+			
+			return "redirect:/";
+		}
+		return "redirect:/";
 	}
 	
 	
