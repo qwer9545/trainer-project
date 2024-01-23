eea87dd045754f4ef41f672c52e692c8d22e8e6a
parfait2 <parkyl0208@naver.com>
2024-01-23 14:53:32 +0900

    [Fix] error fix

--- src/main/java/com/fisa/gym/model/CartRepository.java
+++ src/main/java/com/fisa/gym/model/CartRepository.java
@@ -1,11 +1,17 @@
 package com.fisa.gym.model;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.fisa.gym.model.domain.Cart;
 
 @Repository
-public interface CartRepository extends CrudRepository<Cart, Long>{
+public interface CartRepository extends CrudRepository<Cart, Long> { 
+	
+    @Query("select c.drink.id from Cart c where c.member.id = ?1")
+    List<Long> findDrinkNameByMemberId(Long memberId);
 
 }
--- src/main/java/com/fisa/gym/service/CartService.java
+++ src/main/java/com/fisa/gym/service/CartService.java
@@ -12,15 +12,23 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
-@RequiredArgsConstructor
+@RequiredArgsConstructor // TODO 20 line의 repo가 만들어짐. 흐름 파악.
 public class CartService {
+	
+	// TODO new line 반영
+	// TODO DI 개념, Spring Bean
     private final CartRepository cartRepository;
-	private ModelMapper mapper = new ModelMapper();
+	
+	// TODO new 생성자 -> Bean이 아님
+    private ModelMapper mapper = new ModelMapper();
 
     public boolean insert(CartDTO cartDTO) {
+    	// TODO 에러가 발생해도 클라이언트가 모르는 코드 -> throw e로 던져주기.
         try {
-        	Cart cart = mapper.map(cartDTO, Cart.class);
-        	cartRepository.save(cart);
+        	cartRepository.save(mapper.map(cartDTO, Cart.class));
+        	
+//        	Cart cart = mapper.map(cartDTO, Cart.class);
+//        	cartRepository.save(cart);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
